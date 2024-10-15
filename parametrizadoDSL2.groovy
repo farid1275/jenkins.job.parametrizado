6f582f30beedf052082e3d05d1ba89197c9704b1
farid1275 <farid27orbe@gmail.com>
2024-10-15 16:43:20 +0200

    Update parametrizadoDSL2.groovy

--- parametrizadoDSL2.groovy
+++ parametrizadoDSL2.groovy
@@ -1,13 +1,13 @@
 job('ejemplo3-job-DSL') {
 	description('Job DSL de ejemplo para el curso de Jenkins')
   	scm {
-      		git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
-        		node / gitConfigName('macloujulian')
-        		node / gitConfigEmail('macloujulian@gmail.com')
+      		git('https://github.com/farid1275/jenkins.job.parametrizado.git', 'main') { node ->
+        		node / gitConfigName('mfarid1275')
+        		node / gitConfigEmail('farid27orbe@gmail.com')
       		}
     	} 
   	parameters {
-   		stringParam('nombre', defaultValue = 'Julian', description = 'Parametro de cadena para el Job Booleano')
+   		stringParam('nombre', defaultValue = 'Farid', description = 'Parametro de cadena para el Job Booleano')
       		choiceParam('planeta', ['Mercurio', 'Venus', 'Tierrra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
       		booleanParam('agente', false)
     	}
@@ -19,7 +19,7 @@ job('ejemplo3-job-DSL') {
     		shell("bash jobscript.sh")
     	}
   	publishers {
-      		mailer('macloujulian@gmail.com', true, true)
+      		mailer('farid27orbe@gmail.com', true, true)
       		slackNotifier {
 		  notifyAborted(true)
 		  notifyEveryFailure(true)
